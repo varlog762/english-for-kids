de23961066a1be7545a6a16d3e7b25a284af08e3
Gregory Pereverzev <boot762@gmail.com>
2024-12-16 20:32:13 +0300

    fix: change names

--- src/assets/js/Game.js
+++ src/assets/js/Game.js
@@ -1,212 +1,224 @@
-'use strict';
-import { 
-    createOrUpdateLocalStorage,
-    startGame,
-} from "./utils.js";
+"use strict";
+import { createOrUpdateLocalStorage, startGame } from "./utils.js";
 
 export class Game {
-    /**
-     * Represents a game.
-     * @param {Array} wordCardsCollection - Collection of word cards for the game.
-     */
-    constructor(wordCardsCollection) {
-        this.mainContainer = document.querySelector('.main-container');
-        this.badgesContainer = document.querySelector('.badges-container');
-        this.badgesCollection = [];
-        this.wordCardsCollection = wordCardsCollection;
-        this.playingCard;
-        this.attemtsToWin = this.wordCardsCollection.length;
-        this.attemts = 0;
-        this.bigPlayButton = document.querySelector('.play-button');
-        this.popupContainer = document.querySelector('.popup-container');
+  /**
+   * Represents a game.
+   * @param {Array} wordCardsCollection - Collection of word cards for the game.
+   */
+  constructor(wordCardsCollection) {
+    this.mainContainer = document.querySelector(".main-container");
+    this.badgesContainer = document.querySelector(".badges-container");
+    this.badgesCollection = [];
+    this.wordCardsCollection = wordCardsCollection;
+    this.playingCard;
+    this.attemptsToWin = this.wordCardsCollection.length;
+    this.attempts = 0;
+    this.bigPlayButton = document.querySelector(".play-button");
+    this.popupContainer = document.querySelector(".popup-container");
+  }
+
+  /**
+   * Starts the game.
+   */
+  startGame() {
+    this.playCard();
+    this.playSound("start-game");
+    this.changePlayButton("gameStarted");
+
+    this.mainContainer.addEventListener(
+      "click",
+      this.clickHandlerForCheckAnswer
+    );
+
+    document.body.addEventListener("click", (event) => {
+      if (
+        event.target.closest(".switch") ||
+        event.target.closest(".brgr-menu-item")
+      ) {
+        this.stopGame();
+      }
+    }),
+      { once: true };
+  }
+
+  /**
+   * Handles the click event for checking the answer.
+   * @param {Event} event - The click event.
+   */
+  clickHandlerForCheckAnswer = (event) => this.checkAnswer(event);
+
+  /**
+   * Generates a random index for selecting a card from the collection.
+   * @param {number} collectionLength - The length of the collection.
+   * @returns {number} - The random index.
+   */
+  getRandomCardIndex(collectionLength) {
+    return Math.floor(Math.random() * collectionLength);
+  }
+
+  /**
+   * Plays a card by selecting a random card from the collection.
+   */
+  playCard() {
+    const index = this.getRandomCardIndex(this.wordCardsCollection.length);
+    this.playingCard = this.wordCardsCollection[index];
+
+    setTimeout(() => this.playingCard.parent.playSound(), 1500);
+  }
+
+  /**
+   * Checks the clicked card for correctness and takes appropriate action.
+   * @param {Event} event - The click event.
+   */
+  checkAnswer(event) {
+    const clickedCard = event.target.closest(".unplayed-word");
+
+    if (clickedCard) {
+      if (clickedCard === this.playingCard) {
+        this.indicatePositiveAnswer();
+      } else {
+        this.indicateNegativeAnswer();
+      }
     }
-    
-    /**
-     * Starts the game.
-     */
-    startGame() {
-        this.playCard();
-        this.playSound('start-game');
-        this.changePlayButton('gameStarted');
+  }
 
-        this.mainContainer.addEventListener('click', this.clickHanlerForCheckAnswer);
+  /**
+   * Indicates a positive answer by playing sound, creating a badge, and updating local storage.
+   */
+  indicatePositiveAnswer() {
+    this.playSound("positive-answer");
+    this.createBadgeAndShow("positive");
+    this.attempts++;
+    createOrUpdateLocalStorage(this.playingCard.parent.word, 0, 1, 0);
+    this.playingCard.classList.add("played-card");
+    this.playingCard.classList.remove("unplayed-word");
+    this.checkGameOver();
+  }
 
-        document.body.addEventListener('click', (event) => { 
-            if (event.target.closest('.switch') || event.target.closest('.brgr-menu-item')) {
-                this.stopGame();
-            }
-        }), { once: true };
+  /**
+   * Indicates a negative answer by playing sound, creating a badge, and updating local storage.
+   */
+  indicateNegativeAnswer() {
+    this.playSound("negative-answer");
+    this.createBadgeAndShow("negative");
+    this.attempts++;
+    createOrUpdateLocalStorage(this.playingCard.parent.word, 0, 0, 1);
+  }
 
+  /**
+   * Creates a badge element and displays it in the badges container.
+   * @param {string} answerResult - The result of the answer ('positive' or 'negative').
+   */
+  createBadgeAndShow(answerResult) {
+    const badge = this.createBadge(answerResult);
+    const fragment = document.createDocumentFragment();
+    this.badgesCollection.push(badge);
+    this.badgesCollection.forEach((badge) => fragment.append(badge));
+    this.badgesContainer.innerHTML = "";
+    this.badgesContainer.append(fragment);
+  }
+
+  /**
+   * Creates a badge element based on the answer result.
+   * @param {string} answerResult - The result of the answer ('positive' or 'negative').
+   * @returns {HTMLElement} - The badge element.
+   */
+  createBadge(answerResult) {
+    const badge = document.createElement("div");
+    badge.classList.add(`${answerResult}-badge`);
+
+    return badge;
+  }
+
+  /**
+   * Checks if the game is over and takes appropriate action.
+   */
+  checkGameOver() {
+    if (this.wordCardsCollection.length === 0) {
+      this.gameOver();
+    } else {
+      this.playCard();
     }
+  }
 
-    /**
-     * Handles the click event for checking the answer.
-     * @param {Event} event - The click event.
-     */
-    clickHanlerForCheckAnswer = event => this.checkAnswer(event);
+  /**
+   * Handles the game over state by showing the appropriate popup and reloading the page.
+   */
+  gameOver() {
+    setTimeout(() => {
+      this.bigPlayButton.style.visibility = "hidden";
+      this.badgesContainer.style.visibility = "hidden";
+      this.mainContainer.innerHTML = "";
+      document.body.classList.add("body-not-scroll");
+      if (this.attempts > this.attemptsToWin) {
+        const mistakesNumElem = document.querySelector(".mistakes-number");
+        const mistakesNum = this.attempts - this.attemptsToWin;
+        console.log(mistakesNum, this.attempts, this.attemptsToWin);
 
-    /**
-     * Generates a random index for selecting a card from the collection.
-     * @param {number} collectionLength - The length of the collection.
-     * @returns {number} - The random index.
-     */
-    getRandomCardIndex(collectionLength) {
-        return Math.floor(Math.random() * collectionLength);
+        this.playSound("lose");
+        this.popupContainer.classList.add("popup-container-visible-lose");
+        mistakesNum === 1
+          ? (mistakesNumElem.innerHTML = `1 mistake`)
+          : (mistakesNumElem.innerHTML = `${mistakesNum} mistakes`);
+      } else {
+        this.playSound("win");
+        this.popupContainer.classList.add("popup-container-visible-win");
+      }
+    }, 1000);
+    setTimeout(() => location.reload(), 5000);
+  }
+
+  /**
+   * Plays the sound based on the provided name.
+   * @param {string} name - The name of the sound file.
+   */
+  playSound(name) {
+    const audio = new Audio(`./assets/audio/${name}.mp3`);
+    audio.play();
+  }
+
+  /**
+   * Handles the repeat button click by playing the sound of the current card.
+   */
+  repeatButtonClickHandler = () => this.playingCard.parent.playSound();
+
+  /**
+   * Changes the play button state based on the provided state.
+   * @param {string} state - The state of the play button ('gameStarted' or 'gameStopped').
+   */
+  changePlayButton(state) {
+    if (state === "gameStarted") {
+      this.bigPlayButton.classList.add("play-button-to-repeat");
+      this.bigPlayButton.innerHTML = "repeat";
+      this.bigPlayButton.addEventListener(
+        "click",
+        this.repeatButtonClickHandler
+      );
+    } else {
+      this.bigPlayButton.classList.remove("play-button-to-repeat");
+      this.bigPlayButton.innerHTML = "play";
+
+      this.bigPlayButton.removeEventListener(
+        "click",
+        this.repeatButtonClickHandler
+      );
     }
+  }
 
-    /**
-     * Plays a card by selecting a random card from the collection.
-     */
-    playCard() {
-        const index = this.getRandomCardIndex(this.wordCardsCollection.length);
-        this.playingCard = this.wordCardsCollection[index];
-
-        setTimeout(() => this.playingCard.parent.playSound(), 1500);
-    }
-
-    /**
-     * Checks the clicked card for correctness and takes appropriate action.
-     * @param {Event} event - The click event.
-     */
-    checkAnswer(event) {
-        const clickedCard = event.target.closest('.unplayed-word');
-
-        if (clickedCard) {
-            if (clickedCard === this.playingCard) {
-                this.indicatePositiveAnswer();
-            } else {
-                this.indicateNegativeAnswer();
-            }
-        }
-    }
-
-    /**
-     * Indicates a positive answer by playing sound, creating a badge, and updating local storage.
-     */
-    indicatePositiveAnswer(){
-        this.playSound('positive-answer');
-        this.createBadgeAndShow('positive');
-        this.attemts++;
-        createOrUpdateLocalStorage(this.playingCard.parent.word, 0, 1, 0);
-        this.playingCard.classList.add('played-card');
-        this.playingCard.classList.remove('unplayed-word');
-        this.checkGameOver();
-    }
-
-    /**
-     * Indicates a negative answer by playing sound, creating a badge, and updating local storage.
-     */
-    indicateNegativeAnswer() {
-        this.playSound('negative-answer');
-        this.createBadgeAndShow('negative');
-        this.attemts++;
-        createOrUpdateLocalStorage(this.playingCard.parent.word, 0, 0, 1);
-    }
-
-    /**
-     * Creates a badge element and displays it in the badges container.
-     * @param {string} answerResult - The result of the answer ('positive' or 'negative').
-     */
-    createBadgeAndShow(answerResult) {
-        const badge = this.createBadge(answerResult);
-        const fragment = document.createDocumentFragment();
-        this.badgesCollection.push(badge);
-        this.badgesCollection.forEach(badge => fragment.append(badge));
-        this.badgesContainer.innerHTML = '';
-        this.badgesContainer.append(fragment);
-    }
-
-    /**
-     * Creates a badge element based on the answer result.
-     * @param {string} answerResult - The result of the answer ('positive' or 'negative').
-     * @returns {HTMLElement} - The badge element.
-     */
-    createBadge(answerResult) {
-        const badge = document.createElement('div');
-        badge.classList.add(`${answerResult}-badge`);
-
-        return badge;
-    }
-
-    /**
-     * Checks if the game is over and takes appropriate action.
-     */
-    checkGameOver() {
-        if (this.wordCardsCollection.length === 0) {
-            this.gameOver();
-        } else {
-            this.playCard();
-        }
-    }
-
-    /**
-     * Handles the game over state by showing the appropriate popup and reloading the page.
-     */
-    gameOver() {
-        setTimeout(() => {
-            this.bigPlayButton.style.visibility = 'hidden';
-            this.badgesContainer.style.visibility = 'hidden';
-            this.mainContainer.innerHTML = '';
-            document.body.classList.add('body-not-scroll');
-            if (this.attemts > this.attemtsToWin) {
-                const mistakesNumElem = document.querySelector('.mistakes-number');
-                const mistakesNum = this.attemts - this.attemtsToWin;
-                console.log(mistakesNum, this.attemts, this.attemtsToWin)
-
-                this.playSound('lose');
-                this.popupContainer.classList.add('popup-container-visible-lose');
-                mistakesNum === 1 ? 
-                mistakesNumElem.innerHTML = `1 mistake` : 
-                mistakesNumElem.innerHTML = `${mistakesNum} mistakes`;
-            } else {
-                this.playSound('win');
-                this.popupContainer.classList.add('popup-container-visible-win');
-            }
-        }, 1000);
-        setTimeout(() => location.reload(), 5000);
-    }
-
-    /**
-     * Plays the sound based on the provided name.
-     * @param {string} name - The name of the sound file.
-     */
-    playSound(name) {
-        const audio = new Audio(`./assets/audio/${name}.mp3`);
-        audio.play();
-    }
-
-    /**
-     * Handles the repeat button click by playing the sound of the current card.
-     */
-    repeatButtonClickHandler = () => this.playingCard.parent.playSound();
-    
-    /**
-     * Changes the play button state based on the provided state.
-     * @param {string} state - The state of the play button ('gameStarted' or 'gameStopped').
-     */
-    changePlayButton(state) {
-        if (state === 'gameStarted') {
-            this.bigPlayButton.classList.add('play-button-to-repeat');
-            this.bigPlayButton.innerHTML = 'repeat';
-            this.bigPlayButton.addEventListener('click', this.repeatButtonClickHandler);
-        } else {
-            this.bigPlayButton.classList.remove('play-button-to-repeat');
-            this.bigPlayButton.innerHTML = 'play';
-
-            this.bigPlayButton.removeEventListener('click', this.repeatButtonClickHandler);
-        }
-    }
-
-    /**
-     * Stops the game and resets the necessary variables and event listeners.
-     */
-    stopGame() {
-        this.mainContainer.removeEventListener('click', this.clickHanlerForCheckAnswer);
-        this.playingCard;
-        this.badgesContainer.innerHTML = '';
-        this.badgesCollection = [];
-        this.attemts = 0;
-        this.changePlayButton('gameStoped');
-        this.bigPlayButton.addEventListener('click', startGame, { once:true });
-    }
+  /**
+   * Stops the game and resets the necessary variables and event listeners.
+   */
+  stopGame() {
+    this.mainContainer.removeEventListener(
+      "click",
+      this.clickHandlerForCheckAnswer
+    );
+    this.playingCard;
+    this.badgesContainer.innerHTML = "";
+    this.badgesCollection = [];
+    this.attempts = 0;
+    this.changePlayButton("gameStopped");
+    this.bigPlayButton.addEventListener("click", startGame, { once: true });
+  }
 }
--- src/assets/js/WordCard.js
+++ src/assets/js/WordCard.js
@@ -1,114 +1,143 @@
-'use strict';
+"use strict";
 
-import {
-    Category
-} from "./Category.js";
-import {
-    createAndInsertElement,
-    setMultipleAttributes,
-} from "./utils.js";
+import { Category } from "./Category.js";
+import { createAndInsertElement, setMultipleAttributes } from "./utils.js";
 
 /**
  * Represents a Word Card, which is a subclass of Category.
  */
 export class WordCard extends Category {
-    /**
-    * Constructs a new Word Card object.
-    * @param {string} categoryName - The category name of the word card.
-    * @param {string} ImagePath - The image path of the word card.
-    * @param {string} word - The word on the card.
-    * @param {string} wordTranslation - The translation of the word.
-    * @param {string} soundPath - The path to the sound associated with the word.
-    */
-    constructor(categoryName, ImagePath, word, wordTranslation, soundPath) {
-        super(categoryName, ImagePath);
-        this.word = word;
-        this.wordTranslation = wordTranslation;
-        this.soundPath = soundPath;
-        this.clickedOnTrainMode;
-        this.correctAnswersNum;
-        this.incorrectAnswersNum;
-        this.winRate;
-    }
-    get clickedOnTrainMode() {
-        const objFromLocalStorafe = JSON.parse(localStorage.getItem(this.word)) ?? false;
+  /**
+   * Constructs a new Word Card object.
+   * @param {string} categoryName - The category name of the word card.
+   * @param {string} ImagePath - The image path of the word card.
+   * @param {string} word - The word on the card.
+   * @param {string} wordTranslation - The translation of the word.
+   * @param {string} soundPath - The path to the sound associated with the word.
+   */
+  constructor(categoryName, ImagePath, word, wordTranslation, soundPath) {
+    super(categoryName, ImagePath);
+    this.word = word;
+    this.wordTranslation = wordTranslation;
+    this.soundPath = soundPath;
+    this.clickedOnTrainMode;
+    this.correctAnswersNum;
+    this.incorrectAnswersNum;
+    this.winRate;
+  }
+  get clickedOnTrainMode() {
+    const objFromLocalStorage =
+      JSON.parse(localStorage.getItem(this.word)) ?? false;
 
-        return objFromLocalStorafe ? objFromLocalStorafe.trained : 0;
-    }
-    set clickedOnTrainMode(value) {
-        this.clickedOnTrainMode = value;
-    }
-    get correctAnswersNum() {
-        const objFromLocalStorafe = JSON.parse(localStorage.getItem(this.word)) ?? false;
+    return objFromLocalStorage ? objFromLocalStorage.trained : 0;
+  }
+  set clickedOnTrainMode(value) {
+    this.clickedOnTrainMode = value;
+  }
+  get correctAnswersNum() {
+    const objFromLocalStorage =
+      JSON.parse(localStorage.getItem(this.word)) ?? false;
 
-        return objFromLocalStorafe ? objFromLocalStorafe.correct : 0;
-    }
-    set correctAnswersNum(value) {
-        this.correctAnswersNum = value;
-    }
-    get incorrectAnswersNum() {
-        const objFromLocalStorafe = JSON.parse(localStorage.getItem(this.word)) ?? false;
+    return objFromLocalStorage ? objFromLocalStorage.correct : 0;
+  }
+  set correctAnswersNum(value) {
+    this.correctAnswersNum = value;
+  }
+  get incorrectAnswersNum() {
+    const objFromLocalStorage =
+      JSON.parse(localStorage.getItem(this.word)) ?? false;
 
-        return objFromLocalStorafe ? objFromLocalStorafe.incorrect : 0;
-    }
-    set incorrectAnswersNum(value) {
-        this.incorrectAnswersNum = value;
-    }
-    get winRate() {
-        const total = this.correctAnswersNum + this.incorrectAnswersNum;
+    return objFromLocalStorage ? objFromLocalStorage.incorrect : 0;
+  }
+  set incorrectAnswersNum(value) {
+    this.incorrectAnswersNum = value;
+  }
+  get winRate() {
+    const total = this.correctAnswersNum + this.incorrectAnswersNum;
 
-        return total === 0 ? 0 : Math.round((this.correctAnswersNum / total) * 100);
-    }
-    set winRate(value) {
-        this.winRate = value;
-    }
+    return total === 0 ? 0 : Math.round((this.correctAnswersNum / total) * 100);
+  }
+  set winRate(value) {
+    this.winRate = value;
+  }
 
-    /**
-    * Plays the sound associated with the word card.
-    */
-    playSound() {
-        const audio = new Audio(this.soundPath);
-        audio.play();
-    }
+  /**
+   * Plays the sound associated with the word card.
+   */
+  playSound() {
+    const audio = new Audio(this.soundPath);
+    audio.play();
+  }
 
-    /**
-     * Creates the HTML representation of the word card.
-     * @returns {HTMLElement} The HTML element representing the word card.
-     */
-    createCardHTML() {
-        const wordCard = document.createElement('div');
-        wordCard.parent = this;
+  /**
+   * Creates the HTML representation of the word card.
+   * @returns {HTMLElement} The HTML element representing the word card.
+   */
+  createCardHTML() {
+    const wordCard = document.createElement("div");
+    wordCard.parent = this;
 
-        wordCard.classList.add('card', 'word-card');
-        setMultipleAttributes(wordCard, {
-            'id': this.word,
-            'category': this.categoryName
-        })
+    wordCard.classList.add("card", "word-card");
+    setMultipleAttributes(wordCard, {
+      id: this.word,
+      category: this.categoryName,
+    });
 
-        const frontSide = createAndInsertElement('div', wordCard, 'word-card-front-side'),
-            backSide = createAndInsertElement('div', wordCard, 'word-card-back-side'),
-            wordCardFrontFigure = createAndInsertElement('figure', frontSide, 'card-figure'),
-            rotateButton = createAndInsertElement('button', frontSide, 'card-rotate-btn'),
-            wordCardBackFigure = createAndInsertElement('figure', backSide, 'card-figure');
+    const frontSide = createAndInsertElement(
+        "div",
+        wordCard,
+        "word-card-front-side"
+      ),
+      backSide = createAndInsertElement("div", wordCard, "word-card-back-side"),
+      wordCardFrontFigure = createAndInsertElement(
+        "figure",
+        frontSide,
+        "card-figure"
+      ),
+      rotateButton = createAndInsertElement(
+        "button",
+        frontSide,
+        "card-rotate-btn"
+      ),
+      wordCardBackFigure = createAndInsertElement(
+        "figure",
+        backSide,
+        "card-figure"
+      );
 
+    const wordCardFrontImg = createAndInsertElement(
+        "img",
+        wordCardFrontFigure,
+        "card-img"
+      ),
+      wordCardBackImg = createAndInsertElement(
+        "img",
+        wordCardBackFigure,
+        "card-img"
+      );
+    setMultipleAttributes(wordCardFrontImg, {
+      src: this.ImagePath,
+      alt: this.word,
+    });
+    setMultipleAttributes(wordCardBackImg, {
+      src: this.ImagePath,
+      alt: this.word,
+    });
 
-        const wordCardFrontImg = createAndInsertElement('img', wordCardFrontFigure, 'card-img'),
-            wordCardBackImg = createAndInsertElement('img', wordCardBackFigure, 'card-img');
-        setMultipleAttributes(wordCardFrontImg, {
-            'src': this.ImagePath,
-            'alt': this.word
-        });
-        setMultipleAttributes(wordCardBackImg, {
-            'src': this.ImagePath,
-            'alt': this.word
-        });
+    const cardTitle = createAndInsertElement(
+        "figcaption",
+        wordCardFrontFigure,
+        "card-title"
+      ),
+      cardTranslation = createAndInsertElement(
+        "figcaption",
+        wordCardBackFigure,
+        "card-title"
+      );
+    cardTitle.classList.add("word-card-title");
+    cardTitle.innerHTML = this.word;
+    cardTranslation.innerHTML = this.wordTranslation;
 
-        const cardTitle = createAndInsertElement('figcaption', wordCardFrontFigure, 'card-title'),
-            cardTranslation = createAndInsertElement('figcaption', wordCardBackFigure, 'card-title');
-        cardTitle.classList.add('word-card-title');
-        cardTitle.innerHTML = this.word;
-        cardTranslation.innerHTML = this.wordTranslation;
-
-        return wordCard;
-    }
+    return wordCard;
+  }
 }
